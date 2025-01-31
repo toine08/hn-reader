a3a9487caa6373763bd5947fe9f205200c179bf7
toine08 <riedotoine@gmail.com>
2025-01-31 15:54:49 +0100

    Add TypeScript declarations and interfaces for articles and comments

---
+++ utils/interfaces.ts
@@ -0,0 +1,25 @@
+import { Article } from "./types";
+export interface Comment {
+  id: number;
+  text?: string;
+  by?: string;
+  time?: number;
+  kids?: number[];
+  score: number;  // Added required score field
+  replies?: Comment[];
+  depth?: number;
+}
+
+export interface StoryTypeModalProps {
+  item: number;  // This expects a story ID number
+  kids?: number[];
+  onClose?: () => void;
+}
+export interface ListItemProps {
+  item: Article;  // Change from number to Article
+  storyType: string;
+  type?: "save" | "trash";
+  onPressSave?: () => void;
+  onPressTrash?: () => void;
+  onPressComments?: () => void;
+}
---
+++ utils/types.ts
@@ -0,0 +1,8 @@
+export type Article = {
+  id: number;
+  title: string;
+  url: string;
+  kids: number[];
+  // include other properties of the article here
+};
+
