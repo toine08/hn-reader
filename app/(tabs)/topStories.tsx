d0d209c6d33569ac22f7fc1bae92b06e12c0be24
toine08 <riedotoine@gmail.com>
2025-02-05 10:54:06 +0100

    Refactor topStoriesScreen to replace FlatList with ScrollView, simplifying layout and removing unused code

--- app/(tabs)/topStories.tsx
+++ app/(tabs)/topStories.tsx
@@ -1,102 +1,33 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState} from "react";
 import {
-  Text,
   View,
-  FlatList,
-  StyleSheet,
   TouchableOpacity,
   SafeAreaView,
   Modal,
 } from "react-native";
-import { getStories, storeData } from "@/utils/lib";  // Remove default import
-import ListItem from "@/components/ListItem";
 import StoryTypeModal from "../modal";
 import { FontAwesome } from "@expo/vector-icons";
-import { useColorScheme } from "@/components/useColorScheme.web";
-import LoadingPlaceholder from "@/components/LoadingPlaceholder";
+import { ScrollView } from "@/components/ScrollView";
+import { Article } from "@/utils/types";
 
-const PAGE_SIZE = 20; // Number of items to render at once
 
 export default function topStoriesScreen() {
-  const colorScheme = useColorScheme();
-  const [selectedStoryType, setSelectedStoryType] = useState("topstories");
-  const [stories, setStories] = useState<any[]>([]);
-  const [page, setPage] = useState(1);
   const [modalVisible, setModalVisible] = useState(false);
   const [selectedItem, setSelectedItem] = useState<any>(null);
-  const [loading, setLoading] = useState(false); // Add a loading state
 
 
-  const ITEM_HEIGHT = 50; // Define ITEM_HEIGHT or import it if it's defined in another module
-
-  const handlePressComments = (item: any) => {
+  const handlePressComments = (item: Article) => {
     setSelectedItem(item);
     setModalVisible(true);
   };
 
-  const onPressSave = useCallback((item: any) => { // Add item as a parameter
-    console.log("Save pressed");
-    storeData({
-      key: "article",
-      data: item.id,
-    });
-  }, []);
-
-  const onPressComments = useCallback((item: any) => handlePressComments(item), []); // Add item as a parameter
-
-  const getItemLayout = useCallback((_: any, index: number) => (
-    {length: ITEM_HEIGHT, offset: ITEM_HEIGHT * index, index}
-  ), []);
-
-  useEffect(() => {
-    const loadStories = async () => {
-      setLoading(true)
-      const newStories = await getStories(selectedStoryType, page);
-      setStories((oldStories) => [...oldStories, ...newStories]);
-      setLoading(false)
-    };
-
-    loadStories();
-  }, [selectedStoryType, page]);
-
-  const loadMoreStories = () => {
-    setPage((oldPage) => oldPage + 1);
-  };
-
   return (
     <SafeAreaView className="flex-1 w-fit items-center justify-center bg-white dark:bg-zinc-900">
-      <TouchableOpacity
-        onPress={() => loadMoreStories()}
-        className="hidden top-0 p-3 pt-1 pb-1 flex-row h-fit w-28 z-10 absolute bg-zinc-900 opacity-90 rounded-md items-center justify-between"
-      >
-        <FontAwesome
-          name="refresh"
-          size={24}
-          color={colorScheme === "dark" ? "white" : "black"}
-        />
-        <Text className="text-lg ml-2 text-black dark:text-white">Refresh</Text>
-      </TouchableOpacity>
-      <View className="flex-1 w-full">
-        <FlatList
-          className="bg-white dark:bg-black h-full w-fit"
-          initialNumToRender={5}
-          data={stories.slice(0, (page + 1) * PAGE_SIZE)}
-          keyExtractor={(item) => item.id.toString()} // Ensure unique keys
-          renderItem={({ item }) => (
-            <ListItem
-              type="save"
-              storyType={selectedStoryType}  // Add the required storyType prop
-              item={item}
-              onPressSave={() => onPressSave(item)} // Pass the correct item
-              onPressComments={() => onPressComments(item)} // Pass the correct item
-            />
-          )}
-          ListFooterComponent={loading ? <LoadingPlaceholder /> : null} // Render LoadingPlaceholder when loading
-          onEndReached={loadMoreStories}
-          onEndReachedThreshold={0.2}
-          getItemLayout={getItemLayout}
-        />
-      </View>
+      <ScrollView 
+      story = 'topstories'
+      saveOrTrash="save"
+      onItemSelect={handlePressComments}
+      />
       <Modal
         visible={modalVisible}
         onRequestClose={() => setModalVisible(false)}
