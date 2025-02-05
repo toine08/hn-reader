1f69a68afb0ce06fd69c73f84d4f79ce521b2b14
toine08 <riedotoine@gmail.com>
2025-02-05 10:50:57 +0100

    Refactor jobStoriesScreen to replace FlatList with ScrollView, simplifying state management and removing unused code

--- app/(tabs)/jobStories.tsx
+++ app/(tabs)/jobStories.tsx
@@ -1,89 +1,34 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState} from "react";
 import {
-  Text,
   View,
-  FlatList,
   TouchableOpacity,
   SafeAreaView,
   Modal,
 } from "react-native";
-import { getStories, storeData } from "@/utils/lib";  // Remove default import
-import ListItem from "@/components/ListItem";
+
 import StoryTypeModal from "../modal";
 import { FontAwesome } from "@expo/vector-icons";
-import LoadingPlaceholder from '@/components/LoadingPlaceholder'; // Import your LoadingPlaceholder component
+import { ScrollView } from "@/components/ScrollView";
 
 
 const PAGE_SIZE = 20; // Number of items to render at once
 
 export default function jobStoriesScreen() {
-  const [selectedStoryType, setSelectedStoryType] = useState("jobstories");
-  const [stories, setStories] = useState<any[]>([]);
-  const [page, setPage] = useState(1);
   const [modalVisible, setModalVisible] = useState(false);
   const [selectedItem, setSelectedItem] = useState<any>(null);
-  const [loading, setLoading] = useState(false); // Add a loading state
-
-
-  const ITEM_HEIGHT = 50; // Define ITEM_HEIGHT or import it if it's defined in another module
 
   const handlePressComments = (item: any) => {
     setSelectedItem(item);
     setModalVisible(true);
   };
 
-  const onPressSave = useCallback((item: any) => { // Add item as a parameter
-    console.log("Save pressed");
-    storeData({
-      key: "article",
-      data: item.id,
-    });
-  }, []);
-
-  const onPressComments = useCallback((item: any) => handlePressComments(item), []); // Add item as a parameter
-
-  const getItemLayout = useCallback((_: any, index: number) => (
-    {length: ITEM_HEIGHT, offset: ITEM_HEIGHT * index, index}
-  ), []);
-
-  useEffect(() => {
-    const loadStories = async () => {
-      setLoading(true)
-      const newStories = await getStories(selectedStoryType, page);
-      setStories((oldStories) => [...oldStories, ...newStories]);
-      setLoading(false)
-    };
-
-    loadStories();
-  }, [selectedStoryType, page]);
-
-  const loadMoreStories = () => {
-    setPage((oldPage) => oldPage + 1);
-  };
-
   return (
     <SafeAreaView className="flex-1 w-fit items-center justify-center bg-white dark:dark:bg-zinc-900">
-      <View className="flex-1 w-full">
-        <FlatList
-          className="bg-white dark:bg-black h-full w-fit"
-          initialNumToRender={5}
-          data={stories.slice(0, (page + 1) * PAGE_SIZE)}
-          keyExtractor={(item) => item.id.toString()} // Ensure unique keys
-          renderItem={({ item }) => (
-            <ListItem
-              type="save"
-              storyType={selectedStoryType}  // Add the required storyType prop
-              item={item}
-              onPressSave={() => onPressSave(item)} // Pass the correct item
-              onPressComments={() => onPressComments(item)} // Pass the correct item
+      <ScrollView 
+            story = 'jobstories'
+            saveOrTrash="save"
+            onItemSelect={handlePressComments}
             />
-          )}
-          ListFooterComponent={loading ? <LoadingPlaceholder /> : null} // Render LoadingPlaceholder when loading
-          onEndReached={loadMoreStories}
-          onEndReachedThreshold={0.2}
-          getItemLayout={getItemLayout}
-        />
-      </View>
       <Modal
         visible={modalVisible}
         onRequestClose={() => setModalVisible(false)}
