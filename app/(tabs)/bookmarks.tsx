e3f8ee57520d465b897a2029c9fc5b47ad4aad98
toine08 <riedotoine@gmail.com>
2025-02-05 10:48:57 +0100

    Refactor Bookmarks component to use ScrollView instead of FlatList for improved layout and remove unused code

--- app/(tabs)/bookmarks.tsx
+++ app/(tabs)/bookmarks.tsx
@@ -1,25 +1,18 @@
 import React, { useState } from "react";
-import { useRouter } from "expo-router";
 import {
-  FlatList,
   SafeAreaView,
   Modal,
   View,
   TouchableOpacity
-} from "react-native";
-import { useFocusEffect } from "expo-router";
-import { getStorySaved, removeArticle } from "@/utils/lib";
-import ListItem from "@/components/ListItem";
+} from "react-native";;
 import StoryTypeModal from "../modal";
 import { Article } from "@/utils/types";
 import { FontAwesome } from '@expo/vector-icons';
 import { useColorScheme } from "@/components/useColorScheme";
+import { ScrollView } from "@/components/ScrollView";
 
 export default function Bookmarks() { 
   const colorScheme = useColorScheme();
-  const router = useRouter();
-  const [savedArticles, setSavedArticles] = useState<Article[]>([]);
-  const [refreshing, setRefreshing] = useState(false);
   const [modalVisible, setModalVisible] = useState(false);
   const [selectedItem, setSelectedItem] = useState<Article | null>(null);
 
@@ -28,61 +21,15 @@ export default function Bookmarks() {
     setModalVisible(true);
   };
 
-  const handlePressTrash = async (articleId: number) => {
-    try {
-      const updatedArticles = await removeArticle(articleId);
-      setSavedArticles(updatedArticles);
-    } catch (error) {
-      console.error('Error removing article:', error);
-    }
-  };
 
-  useFocusEffect(
-    React.useCallback(() => {
-      const fetchSavedArticles = async () => {
-        try {
-          const articles = await getStorySaved();
-          setSavedArticles(articles || []);
-        } catch (error) {
-          console.error('Error fetching saved articles:', error);
-        }
-      };
-
-      fetchSavedArticles();
-    }, [])
-  );
-
-  const handleRefresh = async () => {
-    setRefreshing(true);
-    try {
-      const articles = await getStorySaved();
-      setSavedArticles(articles || []);
-    } catch (error) {
-      console.error('Error refreshing saved articles:', error);
-    }
-    setRefreshing(false);
-  };
 
   return (
     <SafeAreaView className="flex-1 w-full bg-white dark:bg-zinc-900">
-      <View className="flex-1 w-full">
-        <FlatList
-          className="bg-white dark:bg-black h-full w-fit"
-          data={savedArticles}
-          keyExtractor={(item) => item.id.toString()} // Ensure unique keys
-          renderItem={({ item }) => (
-            <ListItem
-              type="trash"
-              storyType="bookmarks"  // Add the required storyType prop
-              item={item}
-              onPressTrash={() => handlePressTrash(item.id)}
-              onPressComments={() => handlePressComments(item)}
+            <ScrollView 
+            story="bookmarks"
+            saveOrTrash="trash"
+            onItemSelect={handlePressComments}
             />
-          )}
-          refreshing={refreshing}
-          onRefresh={handleRefresh}
-        />
-      </View>
       <Modal
         visible={modalVisible}
         onRequestClose={() => setModalVisible(false)}
